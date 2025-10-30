8db6f0577ee0fae7f36bbd8b73da0e46a3380c49
Isaac Reister <isaac.reister@gmail.com>
2025-10-30 08:31:18 -0700

    Added a tool that sets up the database correctly, if data is local.

---
+++ ops_code/tools/work_arounds/test_database_add.py
@@ -0,0 +1,18 @@
+#manual add to database for testing
+#author- Isaac Reister
+
+import sqlite3
+from datetime import datetime
+
+db_path = r"/home/funkb/Documents/GliderPP/databases/PPglider_chain_database.db"
+conn = sqlite3.connect(db_path)
+c = conn.cursor()
+
+c.execute("""
+INSERT INTO PPglider_processing_stages
+(glider_type, glider_prefix, glider_number, glider_name, downloaded, date_added, file_downloaded, staged)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+""", ("seaglider", "sg", "454", "Cabot", 1, datetime.now().strftime("%Y%m%d"), r"/home/funkb/Documents/Python/GliderPP/data/Cabot_454_test.nc", 0))
+
+conn.commit()
+conn.close()
